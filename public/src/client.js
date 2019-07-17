b26b0c10704ed758adf868d3200babd51e59e7fb
Rob Bearman <robbear@hyperfine.com>
2019-07-16 19:13:20 -0700

    Remove old code base

--- public/src/client.js
+++
@@ -1,9 +0,0 @@
-import './components/ErrorPage.js';
-import './components/HomePage.js';
-import './components/VersionPage.js';
-import './components/ToolbarTab.js';
-
-import './utilities/Database.js';
-import './utilities/Settings.js';
-import './utilities/SlideShowJSON.js';
-import './utilities/Uuidv4.js';
--- shared/shared.js
+++
@@ -1,8 +0,0 @@
-import AppShell from './AppShell';
-import routes from './routes';
-
-
-/**
- * Single export for all components.
- */
-export { AppShell, routes };
